11a39666bdae210c4087f4659718069c59867a6b
Patrick Livio <patrick.livio@eoc.ch>
2018-11-15 16:23:53 +0100

    Updates

---
+++ precache-manifest.9ee063933a50677649532ca480ce5586.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0a4d67009599ed6abd8",
+    "url": "/Saputello/static/js/runtime~main.b0a4d670.js"
+  },
+  {
+    "revision": "6f91d4b34734dadfc54c",
+    "url": "/Saputello/static/js/main.6f91d4b3.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/Saputello/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "6f91d4b34734dadfc54c",
+    "url": "/Saputello/static/css/main.1243120f.chunk.css"
+  },
+  {
+    "revision": "d5b129b325c9e3430fa52227972742bb",
+    "url": "/Saputello/index.html"
+  }
+];
